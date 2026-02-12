9f080d4fb6d6121157b92b0508fa6a5cd839ddd0
elly-funique <chu.elly@funique.tv>
2026-02-12 15:47:31 +0800

    Added code license part

--- src/master/popup/base_pop_window.cpp
+++ src/master/popup/base_pop_window.cpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) [2026] [Elly/Funique]
+ *
+ * This software is licensed under the [MIT License].
+ * See the LICENSE file in the project root for more information.
+*/
 #include "base_pop_window.h"
 
 BasePopWindow::BasePopWindow(
--- src/master/windows/system_style.h
+++ src/master/windows/system_style.h
@@ -1,1 +1,7 @@
+/*
+ * Copyright (c) [2026] [Elly/Funique]
+ *
+ * This software is licensed under the [MIT License].
+ * See the LICENSE file in the project root for more information.
+*/
 #pragma once
