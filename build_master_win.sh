72e1b5050c302bdd0ec775c1f91ef34de25434b7
elly-funique <chu.elly@funique.tv>
2026-03-02 17:27:58 +0800

    Update toolchain

--- build_master_win.sh
+++ build_master_win.sh
@@ -7,7 +7,7 @@ cd build_master_win
 cp ../Roboto-Medium.ttf .
 
 # Configure CMake
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-Wa,-mbig-obj" -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DCMAKE_CXX_FLAGS="-Wa,-mbig-obj" -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
 
 # Build
 cmake --build . -j $nproc
