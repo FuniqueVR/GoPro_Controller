b086b0af29c540b6d5b48e79ed46f8fe115268af
elly-funique <chu.elly@funique.tv>
2026-03-02 17:34:31 +0800

    Replace build script from sh to bat

--- build_master_win.bat
+++ build_master_win.bat
@@ -1,13 +1,8 @@
-#!/bin/bash
-
-# Create build directory
-mkdir -p build_master_win
+mkdir build_master_win
 cd build_master_win
 
-cp ../Roboto-Medium.ttf .
+xcopy ../Roboto-Medium.ttf .
 
-# Configure CMake
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DCMAKE_CXX_FLAGS="-Wa,-mbig-obj" -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
 
-# Build
 cmake --build . -j $nproc
--- build_server_win.bat
+++ build_server_win.bat
@@ -1,11 +1,6 @@
-#!/bin/bash
-
-# Create a separate build directory for server
-mkdir -p build_server_win
+mkdir build_server_win
 cd build_server_win
 
-# Configure CMake with Toolchain
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-Wa,-mbig-obj" -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=ON -DBUILD_MASTER=OFF
 
-# Build
 cmake --build . -j $nproc
