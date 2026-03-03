2b18316ff67c6e4e954aec81ef5141017bf94b86
elly-funique <chu.elly@funique.tv>
2026-03-03 10:21:58 +0800

    Update

--- build_master_win.bat
+++ build_master_win.bat
@@ -1,6 +1,9 @@
 mkdir build_master_win
 cd build_master_win
 
+wget https://github.com/opencv/opencv/releases/download/4.10.0/opencv-4.10.0-windows.exe
+7z x opencv-4.10.0-windows.exe
+
 xcopy ..\Roboto-Medium.ttf .
 
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
