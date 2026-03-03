990c12c6d95d26a906eddc92ffa42de4d69acaab
elly-funique <chu.elly@funique.tv>
2026-03-03 10:04:13 +0800

    Update readme and script update

--- build_master_win.bat
+++ build_master_win.bat
@@ -1,7 +1,7 @@
 mkdir build_master_win
 cd build_master_win
 
-xcopy ../Roboto-Medium.ttf .
+xcopy ..\Roboto-Medium.ttf .
 
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DCMAKE_CXX_FLAGS="-Wa,-mbig-obj" -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
 
