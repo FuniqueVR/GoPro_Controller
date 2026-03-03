90e633cb5a5a3e71bb49f6e1fe771e72f9e2579c
elly-funique <chu.elly@funique.tv>
2026-03-03 10:08:00 +0800

    Remove gcc flag for msvc build script

--- build_master_win.bat
+++ build_master_win.bat
@@ -3,6 +3,6 @@ cd build_master_win
 
 xcopy ..\Roboto-Medium.ttf .
 
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DCMAKE_CXX_FLAGS="-Wa,-mbig-obj" -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
 
 cmake --build . -j 8
