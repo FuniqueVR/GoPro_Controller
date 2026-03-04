00b69473ccffbb7220ea297a20e64c4709259420
elly-funique <chu.elly@funique.tv>
2026-03-04 11:05:13 +0800

    s

--- src/master/imgui_helper.h
+++ src/master/imgui_helper.h
@@ -14,34 +14,34 @@ struct ImGuiIO;
  * Helping setup the imgui context
  * Such as style or theme stuff
  */
-void setup_imgui();
+extern "C" void setup_imgui();
 /**
  * Setup build-in theme
  */
-void setup_catppuccin_mocha_theme();
+extern "C" void setup_catppuccin_mocha_theme();
 /**
  * Create the imgui context, use it in the begining and out of the loop
  */
-void begin_imgui(struct SDL_Window *window, void *sdl_gl_context, const char *glsl_version);
+extern "C" void begin_imgui(struct SDL_Window *window, void *sdl_gl_context, const char *glsl_version);
 /**
  * Destory the imgui context, use it in the end and out of the loop
  */
-void end_imgui();
+extern "C" void end_imgui();
 /**
  * Create the SDL context
  */
-std::tuple<struct SDL_Window*, const char*> begin_sdl();
+extern "C" std::tuple<struct SDL_Window*, const char*> begin_sdl();
 /**
  * Destory the SDL context
  */
-void end_sdl(struct SDL_Window *window);
+extern "C" void end_sdl(struct SDL_Window *window);
 /**
  * Loop begin
  */
-void begin_loop();
+extern "C" void begin_loop();
 /**
  * Loop end
  * Clear color and swap the chain and stuff
  */
-void end_loop(struct SDL_Window* window, struct ImGuiIO &io);
+extern "C" void end_loop(struct SDL_Window* window, struct ImGuiIO &io);
 
