a5e472f095074d4fcb01257cd81dce538f388baa
elly-funique <chu.elly@funique.tv>
2026-03-09 17:50:06 +0800

    i

--- build_master_win.bat
+++ build_master_win.bat
@@ -4,6 +4,6 @@ cd build_master_win
 
 xcopy ..\Roboto-Medium.ttf . /Y
 
-cmake .. -G "Visual Studio 18 2026" -A x86_64 -DCMAKE_SYSTEM_PROCESSOR=AMD64 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
+cmake .. -G "Visual Studio 18 2026" -A x64 -DCMAKE_SYSTEM_PROCESSOR=AMD64 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
 
-cmake . -G "Visual Studio 18 2026" -A x86_64 --build -j 8
+cmake . -G "Visual Studio 18 2026" -A x64 --build -j 8
