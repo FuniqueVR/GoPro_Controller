e09a819ab82d4cce8236d054a505d9f0305aa10f
elly-funique <chu.elly@funique.tv>
2026-03-09 17:49:48 +0800

    s

--- build_server_win.bat
+++ build_server_win.bat
@@ -1,6 +1,6 @@
 mkdir build_server_win
 cd build_server_win
 
-cmake .. -G "Visual Studio 18 2026" -A x86_64 -DCMAKE_SYSTEM_PROCESSOR=x86_64 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=ON -DBUILD_MASTER=OFF
+cmake .. -G "Visual Studio 18 2026" -A x86_64 -DCMAKE_SYSTEM_PROCESSOR=AMD64 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=ON -DBUILD_MASTER=OFF
 
 cmake . -G "Visual Studio 18 2026" -A x86_64 --build -j 8
