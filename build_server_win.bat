5d04173f67aaf60b98cf9904b41b7846da782af5
elly-funique <chu.elly@funique.tv>
2026-03-09 17:27:00 +0800

    Update

--- build_server_win.bat
+++ build_server_win.bat
@@ -1,6 +1,6 @@
 mkdir build_server_win
 cd build_server_win
 
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=ON -DBUILD_MASTER=OFF
+cmake .. -G "Visual Studio 18 2026" -A x64 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=ON -DBUILD_MASTER=OFF
 
-cmake --build . -j 8
+cmake -G "Visual Studio 18 2026" -A x64 --build . -j 8
