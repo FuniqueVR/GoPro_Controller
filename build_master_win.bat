a21850312992bc3f3ca38635c1262939db36449e
elly-funique <chu.elly@funique.tv>
2026-03-09 18:01:34 +0800

    u

--- build_master_win.bat
+++ build_master_win.bat
@@ -4,6 +4,6 @@ cd build_master_win
 
 xcopy ..\Roboto-Medium.ttf . /Y
 
-cmake .. -DCMAKE_SYSTEM_PROCESSOR="x86_64" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
 
 cmake . --build -j 8
