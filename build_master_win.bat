d5d941243ecf05158571d0005197b60cc19769c2
elly-funique <chu.elly@funique.tv>
2026-03-09 17:48:51 +0800

    s

--- build_master_win.bat
+++ build_master_win.bat
@@ -6,4 +6,4 @@ xcopy ..\Roboto-Medium.ttf . /Y
 
 cmake .. -G "Visual Studio 18 2026" -A x86_64 -DCMAKE_SYSTEM_PROCESSOR=x86_64 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
 
-cmake -G "Visual Studio 18 2026" -A x86_64 --build . -j 8
+cmake . -G "Visual Studio 18 2026" -A x86_64 --build -j 8
--- build_server_win.bat
+++ build_server_win.bat
@@ -3,4 +3,4 @@ cd build_server_win
 
 cmake .. -G "Visual Studio 18 2026" -A x86_64 -DCMAKE_SYSTEM_PROCESSOR=x86_64 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=ON -DBUILD_MASTER=OFF
 
-cmake -G "Visual Studio 18 2026" -A x86_64 --build . -j 8
+cmake . -G "Visual Studio 18 2026" -A x86_64 --build -j 8
