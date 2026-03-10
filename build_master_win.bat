e6b483f62db0b91704091cc5b469c2a6c10c69f9
elly-funique <chu.elly@funique.tv>
2026-03-10 09:53:05 +0800

    Update batch

--- build_master_win.bat
+++ build_master_win.bat
@@ -6,4 +6,4 @@ xcopy ..\Roboto-Medium.ttf . /Y
 
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake/msvc.toolchain.cmake -DBUILD_SERVER=OFF -DBUILD_MASTER=ON
 
-cmake . --build -j 8
+cmake --build . -j 8
