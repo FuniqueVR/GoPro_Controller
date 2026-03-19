ae5dc7ef523f51d8b506d26438f8d8e1afd68abd
elly-funique <chu.elly@funique.tv>
2026-03-19 16:05:27 +0800

    Update button size

--- src/master/windows/inspector.cpp
+++ src/master/windows/inspector.cpp
@@ -306,14 +306,15 @@ void InspectorWindow::draw_status(){
 }
 
 void InspectorWindow::draw_media(){
+    ImGuiStyle& style = ImGui::GetStyle();
+    ImVec2 size = ImGui::GetWindowSize();
+    ImVec2 button_size = ImVec2(size.x / 2.0F - style.ItemSpacing.x, 0);
+
     int32_t camera_ip = master->findCamera(state->current_camera_item);
-    if(ImGui::Checkbox("Create Date Folder", &create_date_folder)){
-        state->update_server();
-    }
     if(ImGui::InputText("Media Download", &state->current_download_location)){
         state->update_server();
     }
-    if(ImGui::Button("All Download")){
+    if(ImGui::Button("All Download", button_size)){
         std::string buffer = state->current_download_location;
         while(buffer.size() > 0 && buffer.at(buffer.size() - 1) == '/'){
             buffer.pop_back();
@@ -326,21 +327,29 @@ void InspectorWindow::draw_media(){
     }
     if(ImGui::IsItemHovered()) ImGui::SetTooltip("Download all exist camera instances");
     ImGui::SameLine();
-    if(ImGui::Button("Single Download")){
+    if(ImGui::Button("Single Download", button_size)){
         
     }
     if(ImGui::IsItemHovered()) ImGui::SetTooltip("Download current select camera instance");
 
-    if(ImGui::Button("Open Home")){
+    if(ImGui::Button("Open Home", button_size)){
         std::system((std::string("open \"") + get_home_directory().string() + std::string("\"")).c_str());
     }
     if(ImGui::IsItemHovered()) ImGui::SetTooltip("Open file explorer for home directory");
     ImGui::SameLine();
-    if(ImGui::Button("Open Select Path")){
+    if(ImGui::Button("Open Select Path", button_size)){
         if(fs::exists(state->current_download_location)){
             std::system((std::string("open \"") + state->current_download_location + std::string("\"")).c_str());
         }
     }
+
+    if(ImGui::Checkbox("Create Date Folder", &create_date_folder)){
+        state->update_server();
+    }
+    if(ImGui::Checkbox("Added Finish File", &put_finish)){
+        state->update_server();
+    }
+
     if(ImGui::IsItemHovered()) ImGui::SetTooltip("Open file explorer for path select directory");
     if(camera_ip >= 0){
         std::shared_ptr<CameraInfo> t = master->getCameras()[camera_ip];
