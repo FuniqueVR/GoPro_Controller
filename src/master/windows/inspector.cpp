da00884c1250dde451a916455fe338232a5b6f11
elly-funique <chu.elly@funique.tv>
2026-04-09 14:20:59 +0800

    Update FPS filter

--- src/master/windows/inspector.cpp
+++ src/master/windows/inspector.cpp
@@ -584,11 +584,13 @@ bool InspectorWindow::conditional_filter_option(int32_t mymodel, int32_t setting
     int32_t preset = state->try_get_status_int32_by_id(PRESET_ID);
 
     int32_t profile = state->try_get_setting_int32_by_id(PROFILES_ID);
+    int32_t res = state->try_get_setting_int32_by_id(VIDEO_RESOLUTION_ID);
     int32_t aspect = state->try_get_setting_int32_by_id(VIDEO_ASPECT_RATIO_ID);
     int32_t fps = state->try_get_setting_int32_by_id(FRAMES_PER_SECOND_ID);
     int32_t antif = state->try_get_setting_int32_by_id(ANTI_FLICKER_V2_ID);
     int32_t videolen = state->try_get_setting_int32_by_id(VIDEO_LENS_ID);
     
+    int32_t res_id = VIDEO_RESOLUTION_VALUE[res];
     int32_t aspect_id = VIDEO_ASPECT_RATIO_VALUE[aspect];
     int32_t fps_id = FRAMES_PER_SECOND_VALUE[fps];
     int32_t videolen_id = VIDEO_LENS_VALUE[videolen];
@@ -693,6 +695,47 @@ bool InspectorWindow::conditional_filter_option(int32_t mymodel, int32_t setting
             }
         }
     }
+    else if(setting_id == FRAMES_PER_SECOND_ID){
+        int32_t value_id = FRAMES_PER_SECOND_VALUE[value_index];
+        if(preset == 0){ // Video
+            if(profile == 0) { // Standard
+                if(aspect_id == 0){ // "4:3"
+                    // 120
+                    if(value_id != 1) return false;
+                }
+                else if(aspect_id == 1){ // "16:9"
+                    if(res_id == 4){ // 2.7K
+                        // 240
+                        if(value_id != 0) return false;
+                    }else{ // 1080
+                        // 30, 60, 120, 240
+                        if(value_id != 8 && value_id != 5 && value_id != 1 && value_id != 0) return false;
+                    }
+                }
+                else if(aspect_id == 3){ // "8:7"
+                    if(res_id == 107){ // 5.3K 8:3
+                        // 24, 30
+                        if(value_id != 10 && value_id != 8) return false;
+                    }else{ // 4K 8:7
+                        // 24, 30, 60
+                        if(value_id != 10 && value_id != 8 && value_id != 5) return false;
+                    }
+                }
+                else if(aspect_id == 4){ // "9:16"
+                    
+                }
+            }
+            else if(profile == 1) { // HDR
+            
+            }
+            else if(profile == 2) { // LOG
+            
+            }
+        }
+    }
+    else if(setting_id == VIDEO_LENS_ID){
+        int32_t value_id = VIDEO_LENS_VALUE[value_index];
+    }
     else if(setting_id == SHUTTER_SPEED_VIDEO_ID){
         if((mymodel&(ANTI_FLICKER_V2_AVA)) > 0){
             int32_t value_id = SHUTTER_SPEED_VIDEO_VALUE[value_index];
@@ -716,8 +759,6 @@ bool InspectorWindow::conditional_filter_option(int32_t mymodel, int32_t setting
             }
         }
     }
-    else if(setting_id == FRAMES_PER_SECOND_ID){
-        int32_t fps_id = FRAMES_PER_SECOND_VALUE[value_index];
-    }
+    
     return true;
 }
