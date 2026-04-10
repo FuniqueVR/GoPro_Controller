cbdbe3a8de7b7f6b67c647c5fb2f8e3eba9b388a
elly-funique <chu.elly@funique.tv>
2026-04-10 10:43:41 +0800

    Finish video len in LOG profile filter

--- src/master/windows/inspector/filter.cpp
+++ src/master/windows/inspector/filter.cpp
@@ -268,11 +268,32 @@ bool InspectorWindow::conditional_filter_option(int32_t mymodel, int32_t setting
                         if(value_id != 10 && value_id != 4 && value_id != 0 && value_id != 3) return false;
                     }
                 }else if(aspect_id == 3){ // 8:7
-
+                    // W
+                    if(value_id != 0) return false;
                 }
             }
             else if(profile == 1){ // LOG
-                
+                if(aspect_id == 1){ // 16:9
+                    if(res_id == 1){ // 4K
+                        if(fps_id == 1){ // 120
+                            // L+, L, W, SV
+                            if(value_id != 10 && value_id != 4 && value_id != 0 && value_id != 3) return false;
+                        }else{
+                            // L+, L, W, SV, HV
+                            if(value_id != 10 && value_id != 4 && value_id != 0 && value_id != 3 && value_id != 9) return false;
+                        }
+                    }else if(res_id == 100){ // 5.3K
+                        if(fps_id == 5){ // 60
+                            // L+, L, W, SV
+                            if(value_id != 10 && value_id != 4 && value_id != 0 && value_id != 3) return false;
+                        }else{
+                            // L+, L, W, SV, HV
+                            if(value_id != 10 && value_id != 4 && value_id != 0 && value_id != 3 && value_id != 9) return false;
+                        }
+                    }
+                }else if(aspect_id == 3){ // 8:7
+                    if(value_id != 0) return false;
+                }
             }
         }
     }
