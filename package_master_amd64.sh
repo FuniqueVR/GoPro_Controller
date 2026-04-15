26c2b605f0ec54e417081074ef29a120f048d0dc
elly-funique <chu.elly@funique.tv>
2026-04-15 10:23:46 +0800

    Fixed typo in build script

--- package_master_amd64.sh
+++ package_master_amd64.sh
@@ -7,12 +7,12 @@ cd package_master_amd64
 mkdir -p go_pro_control_master
 cd go_pro_control_master
 
-cp ../build_master_amd64/master master_linux_amd64
-cp ../build_master_amd64/SourceHanSans-Medium.otf SourceHanSans-Medium.otf
-cp ../build_master_amd64/SourceHanSansK-Medium.otf SourceHanSansK-Medium.otf
-cp ../build_master_amd64/SourceHanSansTC-Medium.otf SourceHanSansTC-Medium.otf
-cp ../LICENSE LICENSE
-cp ../README.md README.md
+cp ../../build_master_amd64/master master_linux_amd64
+cp ../../build_master_amd64/SourceHanSans-Medium.otf SourceHanSans-Medium.otf
+cp ../../build_master_amd64/SourceHanSansK-Medium.otf SourceHanSansK-Medium.otf
+cp ../../build_master_amd64/SourceHanSansTC-Medium.otf SourceHanSansTC-Medium.otf
+cp ../../LICENSE LICENSE
+cp ../../README.md README.md
 
 tar -czvf master_linux_amd64.tar.gz .
 
@@ -23,4 +23,4 @@ mv ./DEBIAN/deb_master_amd64 ./DEBIAN/control
 
 cd ..
 
-dpkg-deb --build go_pro_control_server
+dpkg-deb --build go_pro_control_master
