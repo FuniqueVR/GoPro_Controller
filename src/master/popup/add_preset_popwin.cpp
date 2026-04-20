a8a7dc263dd0fb2284a8f1d3da78ba69adad28c8
elly-funique <chu.elly@funique.tv>
2026-04-20 16:12:56 +0800

    Add preset popup window linking Master manager class with preset CRUD features

--- src/master/popup/add_preset_popwin.cpp
+++ src/master/popup/add_preset_popwin.cpp
@@ -27,29 +27,7 @@ void AddPresetPopup::render(){
         ImGui::TextColored(ImVec4(1, 0, 0, 1), "%s", msg.c_str());
 
         if (ImGui::Button("Confirm")) {
-            bool pass = true;
-            int32_t index = master->findServer(server_ip_buf);
-            if(index == -1){
-                error = "Server does not exist.";
-                pass = false;
-            }
-            if(!all_servers[index]->connected){
-                error = "Server is disconnected.";
-                pass = false;
-            }
-            if(master->findCamera(GetRemoteIPBySerial(camera_serial_buf)) != -1){
-                error = "Camera already added.";
-                pass = false;
-            }
-            if(camera_serial_buf.size() != 3){
-                error = "Serial must be 3 number";
-                pass = false;
-            }
-
-            if(pass){
-                master->command_only(server_ip_buf, "add", std::string(camera_serial_buf));
-                trigger(false);
-            }
+            
         }
         ImGui::SameLine();
         if(ImGui::Button("Cancel")){
