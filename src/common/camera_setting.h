20ac3c62ede0f9d2e6526cb0cf141ff213c25520
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-06-03 17:18:56 +0800

    Update res

--- src/common/camera_setting.h
+++ src/common/camera_setting.h
@@ -33,6 +33,7 @@ const static char* VIDEO_RESOLUTION_STRING[] = {
     "900",
     "4K SPH",
     "8K V2",
+    "1440 V2",
     "5.3K",
     "5.3K 8:7 V2",
     "4K 8:7 V2",
@@ -63,6 +64,7 @@ const static int32_t VIDEO_RESOLUTION_VALUE[] = {
     38, // 900
     39, // 4K SPH
     40, // 8K V2
+    44, // 1440 V2
     100,// 5.3K
     107, // 5.3K 8:7 V2
     108, // 4K 8:7 V2
@@ -93,6 +95,7 @@ const static int32_t VIDEO_RESOLUTION_SUPPORT[] = {
     MODEL_13, // 900
     MODEL_MISSION|MODEL_MAX2, // 4K SPH
     MODEL_MISSION, // 8K V2
+    MODEL_MISSION, // 1440 V2
     MODEL_13_ALL, // 5.3K
     MODEL_13|MODEL_12, // 5.3K 8:7 V2
     MODEL_13|MODEL_12, // 4K 8:7 V2
